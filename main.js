ccf075f90ee4575591892bcb69bd9363b00aef59
Leticia <lsf@protonmail.ch>
2020-05-07 18:54:06 +0200

    quadrati con jquery

--- main.js
+++ main.js
@@ -2,16 +2,17 @@
 // Con jQuery, scrivere un numero random tra 0 e 10 dentro ognuno.
 // BONUS 1: generare la griglia con jQuery
 // BONUS 2: se il numero da inserire è pari scriverlo in rosso, se è dispari in nero, se è 0 in verde
+for (var i = 0; i < 25; i++) {
+    $('.griglia').append('<div class="quadrato"></div>');
+}
 $('.quadrato').each(function(){
     var numeroRandom = generaRandom(0, 10);
     $(this).append(numeroRandom);
 })
 
-
-
 // //aprire un array per i numeri random (25):
 // var arrayRandom = [];
-// //ciclo che gira 25 volte per generare 25 numeri random
+// //ciclo che gira almeno 25 volte per generare 25 numeri random
 // do {
 //     var numeroRandom = generaRandom(0, 10);
 //     arrayRandom.push(numeroRandom);
@@ -19,11 +20,10 @@ $('.quadrato').each(function(){
 //
 // console.log(arrayRandom);
 // $('.quadrato').one('click', function(){
-//     // var posizione = $(this).index();
-//     // console.log(posizione);
-//     // var numeroQuadrato = $(this).eq(posizione);
-//     // console.log(numeroQuadrato);
-//      $(this).append(numeroQuadrato).text();
+//     var posizione = $(this).index();
+//     var numeroQuadrato = $(this).eq(arrayRandom[numeroRandom]);
+//     console.log(numeroQuadrato);
+//      $(this).append(numeroQuadrato);
 // });
 
 
